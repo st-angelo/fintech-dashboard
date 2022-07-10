95cb2cf0bdc86d9350e54ef1ceb38a899c171d90
st-angelo <angelostatescu@gmail.com>
2022-07-10 15:54:24 +0300

    added storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['svelte3', '@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.svelte'],
-      processor: 'svelte3/svelte3',
-    },
-  ],
+  overrides: [{
+    files: ['*.svelte'],
+    processor: 'svelte3/svelte3'
+  }],
   rules: {},
   settings: {
-    'svelte3/typescript': true, // load TypeScript as peer dependency
+    'svelte3/typescript': true // load TypeScript as peer dependency
+
   },
+  extends: ['plugin:storybook/recommended']
 };
